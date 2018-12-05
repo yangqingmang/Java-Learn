592eae98b79c4ecc62ef5af510285391201851f8
yangqingmang <wantianfaxiang@gmail.com>
2018-12-05 15:09:43 +0800

    增加base版本springboot应用

---
+++ springboot-base/src/main/java/com/qmyang/MyApplication.java
@@ -0,0 +1,20 @@
+package com.qmyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ *
+ * @author yangqingmang
+ * @version 1.0
+ * @date Create in 2018-12-05 15:02
+ */
+@SpringBootApplication
+public class MyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyApplication.class, args);
+    }
+
+}
